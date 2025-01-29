435dbd4f3292f3a0d4f17bfe4d46aaccc673cab9
FFerorelli <fabrizio.ferorelli@gmail.com>
2025-01-29 20:10:43 +0100

    little fix

--- Lemons in the Room/Assets/Scripts/VRModeSwitcher.cs
+++ Lemons in the Room/Assets/Scripts/VRModeSwitcher.cs
@@ -1,28 +1,43 @@
 using UnityEngine;
-using UnityEngine.XR;
+using UnityEngine.XR; // Needed for XRSettings
 
 public class VRModeSwitcher : MonoBehaviour
 {
     [SerializeField] private GameObject vrRig;
     [SerializeField] private GameObject pcRig;
 
+    // Reference to the Camera component you want to activate in PC mode
+    [SerializeField] private Camera pcCamera;
+
     void Start()
     {
         // Check if XR is enabled or if a VR headset is detected
         bool vrSupported = XRSettings.isDeviceActive; // or use OVRManager.isHmdPresent if using Oculus Integration
-        Debug.Log(vrSupported +  "--------------------------------");
+        Debug.Log($"VR Supported: {vrSupported}");
 
         if (vrSupported)
         {
             // VR mode
             vrRig.SetActive(true);
             pcRig.SetActive(false);
+
+            // Disable the PC camera
+            if (pcCamera != null)
+            {
+                pcCamera.enabled = false;
+            }
         }
         else
         {
             // PC mode
             vrRig.SetActive(false);
             pcRig.SetActive(true);
+
+            // Enable the PC camera
+            if (pcCamera != null)
+            {
+                pcCamera.enabled = true;
+            }
         }
     }
 }
